453ddc06d823cf035f2c7f72999c07a6e87c6924
Loosineh Hartoonian <98439771+loosineh@users.noreply.github.com>
2022-03-05 08:19:25 -0800

    Added a new case condition to the sumOf function
    to check if the value is negative or positive. If it negative do not add to the sum of array.

--- sumOf.c
+++ sumOf.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
 // Function to return sum of elements
 // The arrays of value: breakfast costs, lunch costs, dinner costs, hotel costs, taxi costs, and parking costs.
-
 float sumOf(float arr[], int size)
 {
     float sum = 0.0;
@@ -8,7 +10,19 @@ float sumOf(float arr[], int size)
     // Iterate through all elements and add them to sum.
     for(int i = 0; i < size; i++)
     {
-        sum += arr[i];
+        if(arr[i] < 0)
+        {
+            printf("Invalid value!");
+        }
+        else
+        {
+            sum += arr[i];
+        }
     }
     return sum;
+}
+
+int main()
+{
+    return 0;
 }
